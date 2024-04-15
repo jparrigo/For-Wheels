62d1dcfdf101eb8ba5eb6d723e7512d589a0e73a
João Arrigo <joaopauloarrigo@gmail.com>
2024-04-15 10:04:28 -0300

    featch: adicionei tela de filtro

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,12 +1,13 @@
 import './Home.css'
 import LeftBar from "../../componentes/LeftBar";
+import Filtro from '../../componentes/Filtro';
 
 function Home() {
   return (
     <div className="home-div">
       <LeftBar />
       <section>
-        
+        <Filtro />
       </section>
     </div>
   )
