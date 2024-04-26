d417582c7164d2baf7d7da24c8ec4b947520f826
João Arrigo <joaopauloarrigo@gmail.com>
2024-04-26 16:48:26 -0300

    feat: adicionei novos componentes a tela de forms

--- frontend/src/componentes/Forms.tsx
+++ frontend/src/componentes/Forms.tsx
@@ -2,6 +2,26 @@ import "./Forms.css";
 import logo from "../assets/logo.png";
 import FieldSlider from "./FieldSlider";
 import FieldTextInput from "./FieldTextInput";
+import { Checkbox, ConfigProvider, Input, Radio, Select, Space } from "antd";
+
+const preferenciaDeUso = [
+  {label: 'Estrada', value: 'highway mpg'},
+  {label: 'Cidade', value: 'city mpg'}
+]
+
+const transmissao = [
+  {label: 'Automatico', value: 'AUTOMATIC'},
+  {label: 'Manual', value: 'MANUAL'},
+  {label: 'Automatizado', value: 'AUTOMATED_MANUAL'},
+  {label: 'Direct Drive', value: 'DIRECT_DRIVE'}
+]
+
+const combustivel = [
+  {label: 'Gasolina', value: 'gasolina'},
+  {label: 'Flex', value: 'flex'},
+  {label: 'Diesel', value: 'diesel'},
+  {label: 'Elétrico', value: 'eletrico'}
+]
 
 export default function Forms() {
   return (
@@ -13,12 +33,61 @@ export default function Forms() {
         </span>
       </div>
       <div className="forms-fields">
-        <FieldTextInput
-          title="Faixa de preço do veículo"
-          firstText="de"
-          secondText="até"
-        />
-        <FieldSlider title="Tamanho" />
+        <section>
+          <h1>Faixa de preço do veículo</h1>
+          <Space.Compact>
+            <Input placeholder="de"/>
+            <Input placeholder="até"/>
+          </Space.Compact>
+        </section>
+        <section>
+          <h1>Preferência de uso</h1>
+          <ConfigProvider theme={{token: {colorText: "#FFFFFF"}}}>
+            <Checkbox.Group defaultValue={['highway mpg']} options={preferenciaDeUso}/>
+          </ConfigProvider>
+        </section>
+        <section>
+          <h1>Tamanho</h1>
+          <Radio.Group value="midsize">
+            <Radio.Button value="midsize">Pequeno</Radio.Button>
+            <Radio.Button value="compact">Médio</Radio.Button>
+            <Radio.Button value="large">Grande</Radio.Button>
+          </Radio.Group>
+        </section>
+        <section>
+          <h1>Transmissão</h1>
+          <ConfigProvider theme={{token: {colorText: "#FFFFFF"}}}>
+            <Checkbox.Group defaultValue={['AUTOMATIC']} options={transmissao}/>
+          </ConfigProvider>
+        </section>
+        <section>
+          <h1>Combustível</h1>
+          <ConfigProvider theme={{token: {colorText: "#FFFFFF"}}}>
+            <Checkbox.Group defaultValue={['gasolina']} options={combustivel}/>
+          </ConfigProvider>
+        </section>
+        <section>
+          <h1>Categoria</h1>
+        </section>
+        <section>
+          <h1>Estilo</h1>
+        </section>
+        <h1>Parte Avançada</h1>
+        <section>
+          <h1>Marca</h1>
+        </section>
+        <section>
+          <h1>Ano</h1>
+        </section>
+        <section>
+          <h1>Potência em HP</h1>
+        </section>
+        <section>
+          <h1>Direção</h1>
+        </section>
+        <section>
+          <h1>Cilíndros</h1>
+        </section>
       </div>
     </div>
   );
