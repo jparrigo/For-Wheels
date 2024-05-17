cf66ad567a68aa62e3f4626dd016c229278161a7
João Arrigo <joaopauloarrigo@gmail.com>
2024-05-17 17:28:13 -0300

    fix: ajustei tela de resultado

--- frontend/src/componentes/Resultado/Resultado.tsx
+++ frontend/src/componentes/Resultado/Resultado.tsx
@@ -3,14 +3,14 @@ import logo from "../../assets/logo.png";
 
 export default function Resultado() {
   return (
-    <section>
-      <div className="forms-logo">
+    <section className='resultado-sec'>
+      <div className="resultado-logo">
         <img src={logo} width={60} />
         <span className="forms-text">
           Resultado do seu filtro de pesquisa
         </span>
       </div>
-      <div>
+      <div className='resultado-div'>
         
       </div>
     </section>
