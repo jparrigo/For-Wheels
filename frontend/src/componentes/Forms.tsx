0e629c19b79a3351a31216a27c52030b6573a8c0
Enrico <enricofarina667@gmail.com>
2024-05-17 17:28:12 -0300

    feat: category and style forms section

--- frontend/src/componentes/Forms.tsx
+++ frontend/src/componentes/Forms.tsx
@@ -1,27 +1,48 @@
 import "./Forms.css";
 import logo from "../assets/logo.png";
-import FieldSlider from "./FieldSlider";
-import FieldTextInput from "./FieldTextInput";
-import { Checkbox, ConfigProvider, Input, Radio, Select, Space } from "antd";
+import { Checkbox, ConfigProvider, Input, Radio, Space } from "antd";
 
 const preferenciaDeUso = [
-  {label: 'Estrada', value: 'highway mpg'},
-  {label: 'Cidade', value: 'city mpg'}
-]
+  { label: "Estrada", value: "highway mpg" },
+  { label: "Cidade", value: "city mpg" },
+];
 
 const transmissao = [
-  {label: 'Automatico', value: 'AUTOMATIC'},
-  {label: 'Manual', value: 'MANUAL'},
-  {label: 'Automatizado', value: 'AUTOMATED_MANUAL'},
-  {label: 'Direct Drive', value: 'DIRECT_DRIVE'}
-]
+  { label: "Automatico", value: "AUTOMATIC" },
+  { label: "Manual", value: "MANUAL" },
+  { label: "Automatizado", value: "AUTOMATED_MANUAL" },
+  { label: "Direct Drive", value: "DIRECT_DRIVE" },
+];
 
 const combustivel = [
-  {label: 'Gasolina', value: 'gasolina'},
-  {label: 'Flex', value: 'flex'},
-  {label: 'Diesel', value: 'diesel'},
-  {label: 'Elétrico', value: 'eletrico'}
-]
+  { label: "Gasolina", value: "gasolina" },
+  { label: "Flex", value: "flex" },
+  { label: "Diesel", value: "diesel" },
+  { label: "Elétrico", value: "eletrico" },
+];
+
+const categoria = [
+  { label: "Comum", value: "Common" },
+  { label: "Crossover", value: "Crossover" },
+  { label: "Exótico", value: "Exotic" },
+  { label: "Tuner de Fábrica", value: "Factory Tuner" },
+  { label: "Hatchback", value: "Hatchback" },
+  { label: "Alta performance", value: "High-Performance" },
+  { label: "Híbrido", value: "Hybrid" },
+  { label: "Luxuoso", value: "Luxury" },
+  { label: "Performance", value: "Performance" },
+];
+
+const estilo = [
+  { label: "Sedan", value: "Sedan" },
+  { label: "SUV", value: "SUV" },
+  { label: "Cupê", value: "Coupe" },
+  { label: "Hatch", value: "Hatchback" },
+  { label: "Conversível", value: "Covertible" },
+  { label: "Picape", value: "Pickup" },
+  { label: "Caminhão", value: "Wagon" },
+  { label: "Mini Van", value: "Minivan" },
+];
 
 export default function Forms() {
   return (
@@ -36,14 +57,17 @@ export default function Forms() {
         <section>
           <h1>Faixa de preço do veículo</h1>
           <Space.Compact>
-            <Input placeholder="de"/>
-            <Input placeholder="até"/>
+            <Input placeholder="de" />
+            <Input placeholder="até" />
           </Space.Compact>
         </section>
         <section>
           <h1>Preferência de uso</h1>
-          <ConfigProvider theme={{token: {colorText: "#FFFFFF"}}}>
-            <Checkbox.Group defaultValue={['highway mpg']} options={preferenciaDeUso}/>
+          <ConfigProvider theme={{ token: { colorText: "#FFFFFF" } }}>
+            <Checkbox.Group
+              defaultValue={["highway mpg"]}
+              options={preferenciaDeUso}
+            />
           </ConfigProvider>
         </section>
         <section>
@@ -56,21 +80,30 @@ export default function Forms() {
         </section>
         <section>
           <h1>Transmissão</h1>
-          <ConfigProvider theme={{token: {colorText: "#FFFFFF"}}}>
-            <Checkbox.Group defaultValue={['AUTOMATIC']} options={transmissao}/>
+          <ConfigProvider theme={{ token: { colorText: "#FFFFFF" } }}>
+            <Checkbox.Group
+              defaultValue={["AUTOMATIC"]}
+              options={transmissao}
+            />
           </ConfigProvider>
         </section>
         <section>
           <h1>Combustível</h1>
-          <ConfigProvider theme={{token: {colorText: "#FFFFFF"}}}>
-            <Checkbox.Group defaultValue={['gasolina']} options={combustivel}/>
+          <ConfigProvider theme={{ token: { colorText: "#FFFFFF" } }}>
+            <Checkbox.Group defaultValue={["gasolina"]} options={combustivel} />
           </ConfigProvider>
         </section>
         <section>
           <h1>Categoria</h1>
+          <ConfigProvider theme={{ token: { colorText: "#FFFFFF" } }}>
+            <Checkbox.Group defaultValue={["comum"]} options={categoria} />
+          </ConfigProvider>
         </section>
         <section>
           <h1>Estilo</h1>
+          <ConfigProvider theme={{ token: { colorText: "#FFFFFF" } }}>
+            <Checkbox.Group defaultValue={["Sedan"]} options={estilo} />
+          </ConfigProvider>
         </section>
         <h1>Parte Avançada</h1>
         <section>
