6c5fb0db134b76c18962cd50442536f06d560968
Guilherme Jorge <guilherme.jorge010@gmail.com>
2024-05-19 17:05:56 -0300

    fix: ajustei a busca de sample_car para não parar caso não ache a primeira vez

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,12 @@
 import pandas as pd
+
 from flask import Flask, request, jsonify, abort
 from flask_cors import CORS, cross_origin
+
 from functions.cosine_similarity import get_cosine_similarity
 from functions.sample_car import get_sample_car_id
 from functions.content_based_recomendation import get_content_based_recommendation
+
 from models.car import Car
 
 
@@ -14,9 +17,14 @@ cors = CORS(app, resources={r"/cars/get-recommendation": {"origins": "*"}})
 app.config["CORS_HEADERS"] = "Content-Type"
 
 
+def abort_data_not_found(data):
+    if data == None:
+        abort(404, message="No request data found")
+
+
 def abort_car_not_found(cars):
-    if cars is None:
-        abort(400, message="No car with filters found")
+    if cars == None:
+        abort(404, message="No car with similar filters found")
 
 
 @app.route("/cars/get-recommendation", methods=["POST", "OPTIONS"])
@@ -36,9 +44,48 @@ def get_recomendation():
         data["style"],
     )
 
-    abort_car_not_found(partial_car)
+    abort_data_not_found(partial_car)
 
     sample_car_id = get_sample_car_id(partial_car, df_cars)
+
+    count = -1
+    while sample_car_id == None:
+        if count == -13:
+            break
+
+        while count > -10:
+            if count == -9:
+                partial_car.category[0] = True
+                count -= 1
+                break
+            if partial_car.category[count] == True:
+                partial_car.category[count] = False
+                count -= 1
+                break
+            count -= 1
+
+        if count == -10:
+            partial_car.city_preference = partial_car.city_preference != True
+
+        if count == -11:
+            partial_car.min_price = 0
+
+        if count == -12:
+            partial_car.max_price *= 1.1
+
+        if count == -13:
+            if partial_car.fuel == "gasoline":
+                partial_car.fuel = "flex-fuel"
+            elif partial_car.fuel == "flex-fuel":
+                partial_car.fuel = "gasoline"
+
+        if count < -9:
+            count -= 1
+
+        sample_car_id = get_sample_car_id(partial_car, df_cars)
+
+    abort_car_not_found(sample_car_id)
+
     recommended_cars = get_content_based_recommendation(
         sample_car_id, df_cars, cosine_sim, n_recommendation=5
     )
